25fd45051aea83bc51cda9e4cab1f8c845b8cb22
Sam Chin <schin@wellesley.edu>
2022-11-13 11:13:29 -0500

    midwork

--- interval.js
+++ interval.js
@@ -4,24 +4,24 @@
 	urls: {
 	//   A0: "A0.mp3",
 	//   C1: "C1.mp3",
-	//   "D#1": "Ds1.mp3",
-	//   "F#1": "Fs1.mp3",
+	//   "Ds1": "Ds1.mp3",
+	//   "Fs1": "Fs1.mp3",
 	//   A1: "A1.mp3",
 	//   C2: "C2.mp3",
-	//   "D#2": "Ds2.mp3",
-	//   "F#2": "Fs2.mp3",
+	//   "Ds2": "Ds2.mp3",
+	//   "Fs2": "Fs2.mp3",
 	//   A2: "A2.mp3",
 	//   C3: "C3.mp3",
-	//   "D#3": "Ds3.mp3",
-	//   "F#3": "Fs3.mp3",
+	//   "Ds3": "Ds3.mp3",
+	//   "Fs3": "Fs3.mp3",
 	  A3: "A3.mp3",
 	  C4: "C4.mp3",
-	  "D#4": "Ds4.mp3",
-	  "F#4": "Fs4.mp3",
+	  "Ds4": "Ds4.mp3",
+	  "Fs4": "Fs4.mp3",
 	  A4: "A4.mp3",
 	  C5: "C5.mp3",
-	  "D#5": "Ds5.mp3",
-	  "F#5": "Fs5.mp3",
+	  "Ds5": "Ds5.mp3",
+	  "Fs5": "Fs5.mp3",
 	  A5: "A5.mp3",
 	//   C6: "C6.mp3",
 	//   "D#6": "Ds6.mp3",
@@ -44,11 +44,12 @@
 
 	const regex = /\d+/g;
 	const notes = ["NULL", "C4", "D4", "E4", "F4", "G4", "A5", "B5", "C5"]
-	const allKeys = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+	const allNotes = ["A", "As", "B", "C", "Cs", "D", "Ds", "E", "F", "Fs", "G", "Gs"]
 
 	window.addEventListener('keydown', checkNote);
 
-	let keySignature = "C";
+
+	let hapticInput = 0;
 
 	let sustain = 1;
 	let trialDelay = 1500;
@@ -56,7 +57,14 @@
 	let intranoteDelay = 0;
 
 	let streakCounter = 0;
+
+	let keySignature = "C";
 	let testNote = randomInterval(1, 8);
+	let majorIntervals =  [2, 2, 1, 2, 2, 2, 1]
+
+	function intervalInKey(base){
+
+	}
 
 	function initialize(e){
 		testNote = randomInterval(1, 8)
@@ -71,6 +79,8 @@
 		trialDelay = $('#trialDelay').val();
 		sensoryDelay = $('#sensoryDelay').val();
 		intranoteDelay = $('#intranoteDelay').val();
+
+		hapticInput =  $('hapticInput').val();
 	}
 
 	function setNote(interval) {
@@ -88,16 +98,16 @@
  		return Math.floor(Math.random() * (max - min + 1) + min)
 	}
 
-	function playNotes(note1, note2){
-		if (intranoteDelay = 0) {
-			sampler.triggerAttackRelease([note1, note2], sustain)
-		}
-		if (intranoteDelay > 0) {
-			setTimeout(() => {sampler.triggerAttackRelease([note1], sustain)}, intranoteDelay);
-			sampler.triggerAttackRelease([note2], sustain)
-		}
-
-	}
+	// function playNotes(note1, note2){
+	// 	if (intranoteDelay = 0) {
+	// 		sampler.triggerAttackRelease([note1, note2], sustain)
+	// 	}
+	// 	if (intranoteDelay > 0) {
+	// 		setTimeout(() => {sampler.triggerAttackRelease([note1], sustain)}, intranoteDelay);
+	// 		sampler.triggerAttackRelease([note2], sustain)
+	// 	}
+	//
+	// }
 
 
 	function checkNote(e) {
